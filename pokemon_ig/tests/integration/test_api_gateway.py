5f7de5d774e9c38087f41e2c9bda248537e231ae
EmilioVejarDiaz <20213tn091@utez.edu.mx>
2024-06-05 11:56:07 -0600

    [ADD] feature/update_publication
    La lambda esta completa, hacen falta pruebas

--- pokemon_ig/tests/integration/test_api_gateway.py
+++ pokemon_ig/tests/integration/test_api_gateway.py
@@ -15,6 +15,8 @@ class TestApiGateway:
     def api_gateway_url(self):
         """ Get the API Gateway URL from Cloudformation Stack outputs """
         stack_name = os.environ.get("AWS_SAM_STACK_NAME")
+        # Agrega impresión para depuración
+        print("Stack Name:", stack_name)
 
         if stack_name is None:
             raise ValueError('Please set the AWS_SAM_STACK_NAME environment variable to the name of your stack')
@@ -43,3 +45,18 @@ class TestApiGateway:
 
         assert response.status_code == 200
         assert response.json() == {"message": "hello world"}
+
+    def test_update_pokemon_lambda(self, api_gateway_url):
+        """ Test the update pokemon lambda """
+        pokemon_id = 1
+        updated_data = {
+            "pokemon_name": "Charizard",
+            "types": "Fire/Flying",
+            "description": "es un charizaurio recs."
+        }
+
+        update_response = requests.post(f"{api_gateway_url}/update_pokemon",
+                                        json={"id_pokemon": pokemon_id, "updated_data": updated_data})
+
+        assert update_response.status_code == 200
+        assert update_response.json() == {"message": "Pokemon updated successfully"}
--- pokemon_ig/update_publication/app.py
+++ pokemon_ig/update_publication/app.py
@@ -1,39 +1,45 @@
+import json
 import pymysql
 
-rds_host = "https://sionpo.clouqoguo4hz.us-east-2.rds.amazonaws.com"
-rds_user = "admin"
-rds_password = "sionpo2024"
-rds_db = "SIONPO"
+host = "sionpo.clouqoguo4hz.us-east-2.rds.amazonaws.com"
+name = "admin"
+password = "sionpo2024"
+db_name = "SIONPO"
 
 
-# def lambda_handler(event, __):
-#     grado = event['pathParameters'].get('grado')
-#     grupo = event['pathParameters'].get('grupo')
-#     profesor = event['pathParameters'].get('profesor')
-#
-#     if grado is None or grupo is None or profesor is None:
-#         return {
-#             'statusCode': 400,
-#             'body': 'Missing required parameters.'
-#         }
-#
-#     insert_into_class(grado, grupo, profesor)
-#
-#     return {
-#         'statusCode': 200,
-#         'body': 'Record inserted successfully.'
-#     }
-#
-#
-# def insert_into_class(grado, grupo, profesor):
-#     connection = pymysql.connect(host=rds_host, user=rds_user, password=rds_password, db=rds_db)
-#
-#     try:
-#         with connection.cursor() as cursor:
-#             insert_query = """
-#             INSERT INTO class (grado, grupo, profesor) VALUES (%s, %s, %s)
-#             """
-#             cursor.execute(insert_query, (grado, grupo, profesor))
-#             connection.commit()
-#     finally:
-#         connection.close()
+def lambda_handler(event, context):
+    pokemon_id = event.get("id_pokemon")
+    updated_data = event.get("updated_data", {})
+
+    connection = pymysql.connect(
+        host=host,
+        user=name,
+        password=password,
+        db=db_name,
+        connect_timeout=5
+    )
+
+    try:
+        with connection.cursor() as cursor:
+            update_query = "UPDATE Pokemon SET "
+            update_query += ", ".join([f"{key}=%s" for key in updated_data.keys()])
+            update_query += " WHERE id_pokemon=%s"
+
+            update_values = list(updated_data.values()) + [pokemon_id]
+
+            cursor.execute(update_query, tuple(update_values))
+            connection.commit()
+
+        response = {
+            "statusCode": 200,
+            "body": "Pokemon updated successfully"
+        }
+    except pymysql.MySQLError as error:
+        response = {
+            "statusCode": 500,
+            "body": str(error)
+        }
+    finally:
+        connection.close()
+
+    return response
