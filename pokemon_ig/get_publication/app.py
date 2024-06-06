81620062c6ec8c6fe8125e3520e11f63632d54e3
FREECS33 <20213tn116@utez.edu.mx>
2024-06-06 10:47:51 -0600

    [ADD] Lambda function for get publication

--- pokemon_ig/get_publication/app.py
+++ pokemon_ig/get_publication/app.py
@@ -23,9 +23,15 @@ def lambda_handler(event, context):
             cursor.execute("SELECT * FROM Pokemon WHERE id_pokemon = %s", (id_pokemon,))
             result = cursor.fetchone()
 
+            if result:
+                columns = [desc[0] for desc in cursor.description]
+                result_dict = dict(zip(columns, result))
+            else:
+                result_dict = {}
+
         response = {
             "statusCode": 200,
-            "body": json.dumps(result, default=str)
+            "body": json.dumps(result_dict, default=str)
         }
     except pymysql.MySQLError as error:
         response = {
