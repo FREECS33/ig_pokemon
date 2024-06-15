9a65b30b340baaedbefc73595be85ff9ba7a7ae3
FREECS33 <20213tn116@utez.edu.mx>
2024-06-14 20:31:33 -0600

    [ADD] CONGIG

--- tests/unit/test_get_publications.py
+++ tests/unit/test_get_publications.py
@@ -1,12 +1,18 @@
 import unittest
 from unittest.mock import patch, MagicMock
 import json
-from get_data_all_pokemon import app
+import pytest
+
+import pymysql
+from botocore.exceptions import ClientError, NoCredentialsError
+
+from get_data_all_pokemon.app import get_secret, lambda_handler
+
 
 class TestLambdaHandler(unittest.TestCase):
-    
-    @patch('app.get_secret')
-    @patch('app.pymysql.connect')
+
+    @patch('get_data_all_pokemon.app.get_secret')
+    @patch('pymysql.connect')
     def test_lambda_handler_success(self, mock_connect, mock_get_secret):
         mock_get_secret.return_value = {
             'host': 'mock-host',
@@ -22,14 +28,14 @@ class TestLambdaHandler(unittest.TestCase):
 
         event = {}
         context = {}
-        
-        response = app(event, context)
-        
+
+        response = lambda_handler(event, context)
+
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(json.loads(response['body']), [{'id': 1, 'name': 'Pikachu'}])
-    
-    @patch('app.get_secret')
-    @patch('app.pymysql.connect')
+
+    @patch('get_data_all_pokemon.app.get_secret')
+    @patch('pymysql.connect')
     def test_lambda_handler_db_connection_error(self, mock_connect, mock_get_secret):
         # Mock secrets
         mock_get_secret.return_value = {
@@ -38,17 +44,18 @@ class TestLambdaHandler(unittest.TestCase):
             'password': 'mock-password'
         }
 
-        mock_connect.side_effect = pymysql.MySQLError("(2003, 'Can\'t connect to MySQL server on \'mock-host\' (10061)')")
+        mock_connect.side_effect = pymysql.MySQLError(
+            "(2003, 'Can\'t connect to MySQL server on \'mock-host\' (10061)')")
 
         event = {}
         context = {}
 
-        response = app(event, context)
+        response = lambda_handler(event, context)
 
         self.assertEqual(response['statusCode'], 503)
         self.assertIn('Cannot connect to database server', response['body'])
 
-    @patch('app.get_secret')
+    @patch('get_data_all_pokemon.app.get_secret')
     def test_lambda_handler_missing_secrets(self, mock_get_secret):
         mock_get_secret.return_value = {
             'username': 'mock-username',
@@ -58,26 +65,26 @@ class TestLambdaHandler(unittest.TestCase):
         event = {}
         context = {}
 
-        response = app(event, context)
+        response = lambda_handler(event, context)
 
         self.assertEqual(response['statusCode'], 500)
         self.assertIn('One or more secrets are missing', response['body'])
-    
-    @patch('app.boto3.session.Session.client')
+
+    @patch('boto3.session.Session.client')
     def test_get_secret_client_error(self, mock_client):
         mock_client_instance = mock_client.return_value
         mock_client_instance.get_secret_value.side_effect = ClientError(
             {"Error": {"Code": "ResourceNotFoundException"}},
             "get_secret_value"
         )
-        
+
         with self.assertRaises(Exception) as context:
             get_secret()
 
         self.assertEqual(context.exception.args[0]['statusCode'], 404)
         self.assertIn('Secret sionpoKeys not found', context.exception.args[0]['body'])
 
-    @patch('app.boto3.session.Session.client')
+    @patch('boto3.session.Session.client')
     def test_get_secret_no_credentials_error(self, mock_client):
         mock_client_instance = mock_client.return_value
         mock_client_instance.get_secret_value.side_effect = NoCredentialsError()
