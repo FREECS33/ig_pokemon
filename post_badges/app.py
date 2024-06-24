4ea375c09829227d5d5a1ad79382d7a31f5fa9ef
Kellguzdi <20213tn114@utez.edu.mx>
2024-06-24 16:10:40 -0600

    Update[Test]: test unit to post_badge

--- post_badges/app.py
+++ post_badges/app.py
@@ -27,13 +27,23 @@ def get_secret():
 def lambda_handler(event, context):
     try:
         body = json.loads(event['body'])
-        if 'badge_name' not in body or 'description' not in body or 'standard_to_get' not in body or 'date_earned' not in body or 'image' not in body:
-            raise ValueError("Missing required fields")
-    except ValueError as error:
+        required_fields = ['badge_name', 'description', 'standard_to_get', 'date_earned', 'image']
+        for field in required_fields:
+            if field not in body:
+                raise ValueError(f"Missing required field: {field}")
+
+        badge_name = body['badge_name']
+        description = json.dumps(body['description'])
+        standard_to_get = body['standard_to_get']
+        date_earned = body['date_earned']
+        image = body['image']
+
+    except (json.JSONDecodeError, ValueError) as error:
         return {
             "statusCode": 400,
             "body": json.dumps({"message": str(error)})
         }
+
     try:
         secrets = get_secret()
     except Exception as error:
@@ -46,6 +56,7 @@ def lambda_handler(event, context):
     name = secrets['username']
     password = secrets['password']
     db_name = "SIONPO"
+
     try:
         connection = pymysql.connect(
             host=host,
@@ -59,47 +70,34 @@ def lambda_handler(event, context):
             "statusCode": 503,
             "body": json.dumps({"message": str(error)})
         }
-    try:
-        badge_name = body['badge_name']
-        description = json.dumps(body['description'])
-        standard_to_get = body['standard_to_get']
-        date_earned = body['date_earned']
-        image = body['image']
 
+    try:
         with connection.cursor() as cursor:
             sql = """
-                    INSERT INTO Badges (
-                        badge_name, description, standard_to_get, date_earned, image
-                    ) VALUES (
-                        %s, %s, %s, %s, %s
-                    )                    
-                """
+                INSERT INTO Badges (
+                    badge_name, description, standard_to_get, date_earned, image
+                ) VALUES (
+                    %s, %s, %s, %s, %s
+                )                    
+            """
             cursor.execute(sql, (
                 badge_name, description, standard_to_get, date_earned, image
             ))
             connection.commit()
+
+            cursor.execute("SELECT * FROM Badges")
+            result = cursor.fetchall()
+
+            response = {
+                "statusCode": 200,
+                "body": json.dumps({"badges": result})
+            }
+
     except pymysql.MySQLError as error:
         return {
             "statusCode": 500,
             "body": json.dumps({"message": str(error)})
         }
-
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM Badges")
-            result = cursor.fetchall()
-
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(result, default=str)
-        }
-
-    except pymysql.MySQLError as error:
-
-        response = {
-            "statusCode": 500,
-            "body": str(error)
-        }
-
     finally:
         connection.close()
 
--- tests/unit/test_badges.py
+++ tests/unit/test_badges.py
@@ -1,1 +1,148 @@
 import unittest
+from unittest.mock import patch, MagicMock
+import json
+
+import pymysql
+from botocore.exceptions import ClientError
+from pymysql import MySQLError
+from post_badges import app
+
+mock_body = {
+    "body": json.dumps({
+        "badge_name": "test_badge",
+        "description": "test_description",
+        "standard_to_get": "test_standard",
+        "date_earned": "2021-01-01",
+        "image": "test_image"
+    })
+}
+
+class TestPostBadges(unittest.TestCase):
+
+    @patch('post_badges.app.get_secret')
+    @patch('post_badges.app.pymysql.connect')
+    def test_lambda_handler_success(self, mock_connect, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+        mock_cursor.fetchall.return_value = [
+            {'id': 1, 'badge_name': 'Example Badge'}]  # Simulando retorno de fetchall()
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_connect.return_value = mock_connection
+
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 200)
+        response_body = json.loads(response['body'])
+        self.assertIsInstance(response_body, dict)
+        self.assertIn('badges', response_body)
+        self.assertIsInstance(response_body['badges'], list)
+        self.assertEqual(response_body['badges'][0]['badge_name'], 'Example Badge')
+
+    @patch('post_badges.app.get_secret')
+    def test_lambda_handler_missing_fields(self, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        event = {
+            "body": json.dumps({
+                "badge_name": "Example Badge"
+            })
+        }
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 400)
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["message"], "Missing required field: description")
+
+    @patch('post_badges.app.get_secret')
+    def test_lambda_handler_invalid_json(self, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        event = {
+            "body": "invalid-json"
+        }
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 400)
+        response_body = json.loads(response['body'])
+        self.assertIn("Expecting value", response_body["message"])
+
+    @patch('post_badges.app.get_secret')
+    @patch('post_badges.app.pymysql.connect')
+    def test_lambda_handler_db_connection_error(self, mock_connect, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        mock_connect.side_effect = pymysql.OperationalError(
+            "(2003, 'Can\'t connect to MySQL server on \'mock-host\' (timed out)')")
+
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 503)
+        response_body = json.loads(response['body'])
+        self.assertIn("Can't connect to MySQL server on 'mock-host'", response_body["message"])
+
+    @patch('post_badges.app.get_secret')
+    def test_lambda_handler_get_secret_fail(self, mock_get_secret):
+        mock_get_secret.side_effect = Exception("Test secret error")
+
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 500)
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["message"], "Test secret error")
+
+    @patch('post_badges.app.get_secret')
+    @patch('post_badges.app.pymysql.connect')
+    def test_lambda_handler_insert_fail(self, mock_connect, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor.execute.side_effect = pymysql.MySQLError("Insert failed")
+        mock_connect.return_value = mock_connection
+
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 500)
+        response_body = json.loads(response['body'])
+        self.assertIn("Insert failed", response_body["message"])
+
+    
