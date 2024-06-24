563abf76047e397a80c19d94f3dfe0e1cf03b9e0
Kellguzdi <20213tn114@utez.edu.mx>
2024-06-24 01:42:11 -0600

    Update[test]: change all test to delete_reaction with 100% coverage

--- tests/unit/test_delete_reaction.py
+++ tests/unit/test_delete_reaction.py
@@ -1,98 +1,181 @@
-import json
 import unittest
 from unittest.mock import patch, MagicMock
-from botocore.exceptions import ClientError, NoCredentialsError, PartialCredentialsError
-from delete_reaction.app import get_secret, lambda_handler
+import pymysql
+from botocore.exceptions import ClientError
+import json
+from pymysql import MySQLError
 
+from delete_reaction import app
+
+mock_body = {
+    "body": json.dumps({"id_interaction": 1})
+}
 
 class TestLambdaHandler(unittest.TestCase):
 
     @patch('delete_reaction.app.get_secret')
-    @patch('pymysql.connect')
+    @patch('delete_reaction.app.pymysql.connect')
     def test_lambda_handler_success(self, mock_connect, mock_get_secret):
         mock_get_secret.return_value = {
-            'host': 'example-host',
-            'username': 'example-user',
-            'password': 'example-pass'
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
         }
 
         mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor.execute.return_value = 1
         mock_connect.return_value = mock_connection
-        mock_cursor = mock_connection.cursor.return_value
-        mock_cursor.rowcount = 1
 
-        event = {
-            'body': json.dumps({
-                'id_interaction': '1234'
-            })
-        }
+        event = mock_body
         context = {}
 
-        response = lambda_handler(event, context)
+        response = app.lambda_handler(event, context)
 
         self.assertEqual(response['statusCode'], 200)
-        data = json.loads(response['body'])
-        self.assertEqual(data['message'], 'Interaction deleted successfully')
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["message"], "Interaction deleted successfully")
 
     @patch('delete_reaction.app.get_secret')
-    @patch('pymysql.connect')
-    def test_lambda_handler_missing_parameter(self, mock_connect, mock_get_secret):
+    def test_lambda_handler_missing_id_interaction(self, mock_get_secret):
         mock_get_secret.return_value = {
-            'host': 'example-host',
-            'username': 'example-user',
-            'password': 'example-pass'
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
         }
 
         event = {
-            'body': json.dumps({})
+            "body": json.dumps({})
         }
         context = {}
 
-        response = lambda_handler(event, context)
+        response = app.lambda_handler(event, context)
 
         self.assertEqual(response['statusCode'], 400)
-        data = json.loads(response['body'])
-        self.assertIn('message', data)
-        self.assertIn('Missing id_interaction in request body', data['message'])
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["error"], "Missing id_interaction in request body")
 
-    @patch('boto3.session.Session.client')
-    def test_get_secret_client_error(self, mock_boto_client):
-        error_response = {
-            'Error': {
-                'Code': 'ResourceNotFoundException',
-                'Message': 'Secrets Manager can\'t find the specified secret.'
-            }
+    @patch('delete_reaction.app.get_secret')
+    def test_lambda_handler_invalid_json(self, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
         }
-        mock_boto_client.return_value.get_secret_value.side_effect = ClientError(
-            error_response, 'get_secret_value'
-        )
 
-        with self.assertRaises(Exception) as context:
-            get_secret()
+        event = {
+            "body": "invalid-json"
+        }
+        context = {}
 
-        self.assertEqual(context.exception.args[0]['statusCode'], 404)
-        self.assertIn('Secret sionpoKeys not found', context.exception.args[0]['body'])
+        response = app.lambda_handler(event, context)
 
-    @patch('boto3.session.Session.client')
-    def test_get_secret_no_credentials_error(self, mock_boto_client):
-        mock_boto_client.return_value.get_secret_value.side_effect = NoCredentialsError()
+        self.assertEqual(response['statusCode'], 400)
+        response_body = json.loads(response['body'])
+        self.assertIn("Expecting value", response_body["error"])
 
-        with self.assertRaises(Exception) as context:
-            get_secret()
+    @patch('delete_reaction.app.get_secret')
+    @patch('delete_reaction.app.pymysql.connect')
+    def test_lambda_handler_db_connection_error(self, mock_connect, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
 
-        self.assertEqual(context.exception.args[0]['statusCode'], 500)
-        self.assertIn('AWS credentials not found', context.exception.args[0]['body'])
+        mock_connect.side_effect = pymysql.OperationalError("(2003, 'Can\'t connect to MySQL server on \'mock-host\' (timed out)')")
 
-    @patch('boto3.session.Session.client')
-    def test_get_secret_partial_credentials_error(self, mock_boto_client):
-        mock_boto_client.return_value.get_secret_value.side_effect = PartialCredentialsError(provider='aws', cred_var='secret_access_key')
+        event = mock_body
+        context = {}
 
-        with self.assertRaises(Exception) as context:
-            get_secret()
+        response = app.lambda_handler(event, context)
 
-        self.assertEqual(context.exception.args[0]['statusCode'], 500)
-        self.assertIn('Incomplete AWS credentials', context.exception.args[0]['body'])
+        self.assertEqual(response['statusCode'], 503)
+        response_body = json.loads(response['body'])
+        self.assertIn("Can't connect to MySQL server on 'mock-host'", response_body["error"])
 
+    @patch('delete_reaction.app.get_secret')
+    def test_lambda_handler_get_secret_fail(self, mock_get_secret):
+        mock_get_secret.side_effect = Exception("Test secret error")
 
-if __name__ == '__main__':
-    unittest.main()
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 500)
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["error"], "Test secret error")
+
+    @patch('delete_reaction.app.get_secret')
+    @patch('delete_reaction.app.pymysql.connect')
+    def test_lambda_handler_delete_interaction_fail(self, mock_connect, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor.execute.side_effect = pymysql.MySQLError("Delete failed")
+        mock_connect.return_value = mock_connection
+
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 400)
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["error"], "Error deleting interaction table")
+
+    @patch('delete_reaction.app.get_secret')
+    @patch('delete_reaction.app.pymysql.connect')
+    def test_lambda_handler_interaction_not_found(self, mock_connect, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor.execute.return_value = 0  # Mocking interaction not found
+        mock_connect.return_value = mock_connection
+
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 404)
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["error"], "Interaction not found")
+
+    @patch('delete_reaction.app.get_secret')
+    @patch('delete_reaction.app.pymysql.connect')
+    def test_lambda_handler_unexpected_error(self, mock_connect, mock_get_secret):
+        mock_get_secret.return_value = {
+            'host': 'mock-host',
+            'username': 'mock-username',
+            'password': 'mock-password'
+        }
+
+        mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor.execute.side_effect = Exception("Unexpected error")  # Mocking unexpected error
+        mock_connect.return_value = mock_connection
+
+        event = mock_body
+        context = {}
+
+        response = app.lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 500)
+        response_body = json.loads(response['body'])
+        self.assertEqual(response_body["error"], "An unexpected error occurred")
