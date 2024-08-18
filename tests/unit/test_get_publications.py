ca249a4ed0d5acc0ba19d144e1f71fd9540bcf67
FREECS33 <20213tn116@utez.edu.mx>
2024-08-17 21:54:33 -0600

    [FIX] Fixign tokens

--- tests/unit/test_get_publications.py
+++ tests/unit/test_get_publications.py
@@ -7,8 +7,8 @@ from get_data_all_pokemon.app import get_secret, lambda_handler
 
 event = {
     "headers": {
-        #Actualizar token con uno que sea valido y no este expirado para ejecutar las pruebas unitarias
-        "Authorization": "Bearer eyJraWQiOiI1YW9nb3RzZk9PTFF1Mm1JNzJOVEV3VnRqZmJqUWFpUzE2d2pPT25kTkVzPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3MTBiYjVlMC01MGQxLTcwZTQtNTY3OS1hMzlkMWIxYjUyZWIiLCJjb2duaXRvOmdyb3VwcyI6WyJ1c2VyIl0sImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0yX05EWFpPRzdEUSIsImNsaWVudF9pZCI6IjVzNWMxb2Zwa3EzMGdrYnQ2MXExaGRpY2ZkIiwib3JpZ2luX2p0aSI6IjNhZjc3ZGIyLTE2MmYtNDM4MS1hMWQxLTYzNGM3NjcyMzU5YSIsImV2ZW50X2lkIjoiZjE3NTU5ZjUtZWI1Ni00ZTBjLWFkYjctMDg4NjJkMmE0ZTEyIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTcyMzk1MjA0OSwiZXhwIjoxNzIzOTU1NjQ5LCJpYXQiOjE3MjM5NTIwNDksImp0aSI6IjNmMzJkOTM5LTRmMWEtNDQxNC05OTAyLWNhZWY0MjJlYjFiOSIsInVzZXJuYW1lIjoic2VyaW8ifQ.RZ-J5SejgdrfyVgow1IJVXnHmS1mF4Z7qfOd4RjJeiIM6Pc8ThGax51DGBjSh3GQCrICW8LzksfAgXsZWinPSUFbbt8nBFWkMPjmk6tPs3z_JdzbKJ3XbOsV3uFU0dBEVi5D0-BxQ6xjg80hCWZQjJTen21V8LqWV_SzWoHpT-D7tcdRwaAIFhImn2sEZpvr5IPb2gBC5p1dDB7s_9gao0G4kvZ00xMHNCdNeSwcngHQny2b4G51_W-jYKV5W8oZNg785ss7DkRmFqXUwidAU_e8u_wI-j7uth6GQBywz4ceTOzXfCCot7S4wXI1DtWMb_bQtlVk-Y0FM5FUU_cmBg"
+        #Actualizar token con uno que sea valido y no este expirado para ejecutar las pruebas unitarias (Access Token)
+        "Authorization": "Bearer "
     },
     "body": json.dumps({
         "id_user": 1
@@ -333,20 +333,8 @@ class TestLambdaHandler(unittest.TestCase):
     def test_lambda_handler_invalid_audience_token(self, mock_connect, mock_get_secret):
         event_invalid_audience = {
             "headers": {
-                "Authorization": "Bearer eyJraWQiOiJcL1hQaHAzQ1UzYkNBQnp2dnM1ZTdLOG5hOGFtR2ZzSlRvZDhhYlYxc2dLUT0iLCJhbG"
-                                 "ciOiJSUzI1NiJ9.eyJzdWIiOiI3MTBiYjVlMC01MGQxLTcwZTQtNTY3OS1hMzlkMWIxYjUyZWIiLCJjb2duaX"
-                                 "RvOmdyb3VwcyI6WyJ1c2VyIl0sImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml"
-                                 "0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl9ORFhaT0c3RFEiLCJjb2duaXRvOnVz"
-                                 "ZXJuYW1lIjoic2VyaW8iLCJwaWN0dXJlIjoiaHR0cDpcL1wvZXhhbXBsZS5jb21cL3BpY3R1cmUuanBnIiwib"
-                                 "3JpZ2luX2p0aSI6IjNhZjc3ZGIyLTE2MmYtNDM4MS1hMWQxLTYzNGM3NjcyMzU5YSIsImF1ZCI6IjVzNWMxb2"
-                                 "Zwa3EzMGdrYnQ2MXExaGRpY2ZkIiwiZXZlbnRfaWQiOiJmMTc1NTlmNS1lYjU2LTRlMGMtYWRiNy0wODg2MmQ"
-                                 "yYTRlMTIiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTcyMzk1MjA0OSwiZXhwIjoxNzIzOTU1NjQ5"
-                                 "LCJpYXQiOjE3MjM5NTIwNDksImp0aSI6ImNjNmIwZTRhLTBjMGItNDlmMi05MDU1LTIyYjI5NjdjNzkyNyIsI"
-                                 "mVtYWlsIjoiYWxlamFuZHJvaWJhcnJhYnJpdG9AZ21haWwuY29tIn0.LOcF9zsOcGrx3pWX0aGOrJYffGo4Mh"
-                                 "6yQSI0tE67c9GR8PUdcDJb7ROLBo3ghkF6b_9Ig9u8DS0jYIk7KuTpysx0EZey2bQTIKTTAfH0vvXT4otUsDe"
-                                 "DnmzBKRM8at89ny3P9jnsdQz1JkGWXUs8ZpdHtUgUupRmGmO92tfjs7ndBYptk2_f-yPV8Cb8yO5n1pcKqyoP"
-                                 "5SwJlS9YcHW3TqmVeJflWF-b2WRJqIROeXy5psPzfIMMZZtemk3jOVUYV0sB1DKU4FElEbCFtUBQRPznzX2h9"
-                                 "H5T9t_7KDQ0uChRpYRTD26w92Bby8kzgJR6_Qfl_e-lrKh6wTAYtVBK3w"
+                #Actualizar con un token valido, que no haya expirado (Id token)
+                "Authorization": "Bearer "
             }
         }
         context = {}
@@ -361,19 +349,8 @@ class TestLambdaHandler(unittest.TestCase):
     def test_lambda_handler_expired_token(self, mock_connect, mock_get_secret):
         event_expired_token = {
             "headers": {
-                "Authorization": "Bearer eyJraWQiOiI1YW9nb3RzZk9PTFF1Mm1JNzJOVEV3VnRqZmJqUWFpUzE2d2pPT25kTkVzPSIsIm"
-                       "FsZyI6IlJTMjU2In0.eyJzdWIiOiI3MTBiYjVlMC01MGQxLTcwZTQtNTY3OS1hMzlkMWIxYjUyZWIiLCJ"
-                       "jb2duaXRvOmdyb3VwcyI6WyJ1c2VyIl0sImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0"
-                       "LTIuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0yX05EWFpPRzdEUSIsImNsaWVudF9pZCI6IjVzNWMxb2Zwa"
-                       "3EzMGdrYnQ2MXExaGRpY2ZkIiwib3JpZ2luX2p0aSI6Ijg2NGZmNjMwLTcyM2ItNDFkMS1hOGIwLTNmZD"
-                       "hkYTZjYzA0NyIsImV2ZW50X2lkIjoiN2E0N2JjMGMtYzU1Ni00YzFmLWI4OGQtMzgzZWNlZmJiM2FmIiwi"
-                       "dG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsIm"
-                       "F1dGhfdGltZSI6MTcyMzk0ODA5MCwiZXhwIjoxNzIzOTUxNjkwLCJpYXQiOjE3MjM5NDgwOTAsImp0aSI6"
-                       "IjZhOGE5MzBlLTBiMjctNGJhOC05ZWQ3LTk2ZmY5Y2U2ZjhmMSIsInVzZXJuYW1lIjoic2VyaW8ifQ.Jk7"
-                       "US_dawuJkljCr4zvwz1EJYWfGg6xT99bSgtFruyGJeO7ABEJ8t6a4PlxSD8lucWZQd5Xmae2k6DnmOzAd8"
-                       "riHcF4d1Qo5Ede1Mn9XhCk-cameu3-hTUAlYel4YKHlSSyIaU-uimMzHpSZcB9XphGu81yXAVh6jNpOovM"
-                       "FhQTTZH4AkXQiFTtKE4X7jFXQtUgEjAxteMQkCVSIPvTqbIAPSw_8lT4qTRzh_N4FkslPV2XHKxPQFP8fh"
-                       "RmtrYPhZWIDcNLdrlZ-PFD2F_XNQ-Npt1t533IvrFo2a0z4Q2tbg-v9iHcvHXkcj3cSGq5KxeIKjeG8E_IZyF6IFQcCPg"
+                #Actualizar con un token valido y que haya expirado (Access token)
+                "Authorization": "Bearer "
             }
         }
         context = {}
@@ -388,7 +365,8 @@ class TestLambdaHandler(unittest.TestCase):
     def test_lambda_handler_invalid_permits(self, mock_connect, mock_get_secret):
         event_token = {
             "headers": {
-                "Authorization": "Bearer eyJraWQiOiI1YW9nb3RzZk9PTFF1Mm1JNzJOVEV3VnRqZmJqUWFpUzE2d2pPT25kTkVzPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJhMWFiNjVmMC05MDYxLTcwODQtODIxYS04MmUyYWI2OTIwMWYiLCJjb2duaXRvOmdyb3VwcyI6WyJhZG1pbSJdLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl9ORFhaT0c3RFEiLCJjbGllbnRfaWQiOiI1czVjMW9mcGtxMzBna2J0NjFxMWhkaWNmZCIsIm9yaWdpbl9qdGkiOiJmMjY3YmJkNC0xMDUzLTQ1ODgtYWUyOS0wYjFlNGYxNzkyNjUiLCJldmVudF9pZCI6IjA4ZjdiOWYyLTk3ZDUtNDcyNS1iNWNiLWRhNTU2YmJlMzIxNSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE3MjM5NTI0MDIsImV4cCI6MTcyMzk1NjAwMiwiaWF0IjoxNzIzOTUyNDAyLCJqdGkiOiJkODY5ZTVlYy01MjNjLTQ5MjctOWFlOS04NjdlODQyM2JkOWIiLCJ1c2VybmFtZSI6InplbCJ9.t_YMvV-GX43_dulCzCNNzXoWttIAhVQvvSNpALg8bARUntAOjbGOkTDSjx0jvBVc0uzhAZbPlOi2zrkcl0mI0SuNnX5h7Hf1lt1JuxWYuutvdfhflc6ipYTAqX8asU6-1MYXLNHm-2uwD2yV57_kJsRqrdQWJiTGNYFuh4aeBsoZxjwMOfb4uUotyEZyDbl5qAbIJTTHdjEcdIqo1vQEG3Et3uHmQbEGcwweTuUiSz33IELY-47zwrja7gue5GZyOIf-s7IeK5YzIy2KiQiHCS-2afdB7G5K9ZxbzC2EKZu6u5iMmSVa9Tj7Bk5DKfmHNi2a4s9qUa0B9JHZBGeaWA"
+                #Actualizar con un token que no contenga los roles permitidos (Access Token)
+                "Authorization": "Bearer "
             }
         }
         context = {}
