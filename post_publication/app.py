880194abf68446282dd685977a04ed1c10b765ff
FREECS33 <20213tn116@utez.edu.mx>
2024-08-18 22:42:43 -0600

    [UPDATE] unit tests

--- post_publication/app.py
+++ post_publication/app.py
@@ -2,6 +2,7 @@ import json
 import pymysql
 import boto3
 import jwt
+from jwt import PyJWKClient
 from botocore.exceptions import ClientError, NoCredentialsError, PartialCredentialsError
 
 
@@ -66,7 +67,59 @@ def get_secret():
         })
 
 
+keys_url = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_NDXZOG7DQ/.well-known/jwks.json"
+
+
 def lambda_handler(event, context):
+    try:
+        if "headers" not in event or "Authorization" not in event["headers"]:
+            return {
+                "statusCode": 400,
+                "body": json.dumps("Authorization header is missing")
+            }
+        auth_header = event["headers"]["Authorization"]
+
+        if not auth_header.startswith("Bearer "):
+            return {
+                "statusCode": 400,
+                "body": json.dumps("Authorization header must start with 'Bearer '")
+            }
+
+        token = auth_header.split(' ')[1]
+
+        jwt_client = PyJWKClient(keys_url)
+
+        public_key = jwt_client.get_signing_key_from_jwt(token)
+
+        decoded_token = jwt.decode(token, key=public_key, algorithms=["RS256"])
+
+        user_groups = decoded_token.get('cognito:groups', [])
+
+        if "user" not in user_groups:
+            return {
+                "statusCode": 403,
+                "body": json.dumps("Access Denied: Insufficient permits")
+            }
+    except jwt.DecodeError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps("Invalid token")
+        }
+    except jwt.ExpiredSignatureError:
+        return {
+            "statusCode": 401,
+            "body": json.dumps("Token has expired")
+        }
+    except jwt.InvalidTokenError as e:
+        return {
+            "statusCode": 401,
+            "body": json.dumps(f"Invalid token: {str(e)}")
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(f"Internal server error: {str(e)}")
+        }
     try:
         body = json.loads(event['body'])
         required_fields = ['pokemon_name', 'abilities', 'types', 'description', 'image']
@@ -141,7 +194,7 @@ def lambda_handler(event, context):
                 dislikes_count, creation_update_date, fk_id_user_creator
             ))
             connection.commit()
-        
+
         response = {
             "statusCode": 200,
             'headers': {
